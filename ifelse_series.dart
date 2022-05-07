d657e0f3864e1e44539bd8bfcbcdc0f76f689a6a
Ar Bin <46072710+pokharelarbin@users.noreply.github.com>
2022-05-07 11:36:22 +0545

    conditional operator  + if else series

---
+++ ifelse_series.dart
@@ -0,0 +1,40 @@
+//dart conditional operator
+
+void main(){
+	int a = 5;
+	int b = 4;
+	int x = null;
+	int y = 8;
+	var res = a>b ? "Greater":"Smaller";
+	var res1 = x ?? y;
+	print(res);
+	//if condition
+	if(a>4){
+		print(a);
+	}
+
+	//if-else condition
+	if (a>4){
+		print(a);
+	}
+	else{
+		print(b);
+	}
+
+	//elseif ladder
+	if(a>5){
+		print(a+b);
+	}
+	elseif (a<2){
+		print(b-a);
+	}
+	else{
+		print(a);
+	}
+
+	//if else and logical operator
+	if (a>4) && (a>2) {
+		print (a+b);
+	}
+}
+
