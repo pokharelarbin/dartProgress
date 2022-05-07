0e0740bbda97516ff84386756163f44c888480b5
Ar Bin <46072710+pokharelarbin@users.noreply.github.com>
2022-05-07 11:03:52 +0545

    data types

---
+++ dartTypes.dart
@@ -0,0 +1,25 @@
+//number
+int a =1;
+double b=1.5;
+var a1 = num.parse("1");
+
+//string
+String str = "this is string literals"
+
+//boolean
+String str1 = "String";
+String str2 = "String";
+bool val = (str1==str2);
+print(val);
+
+//list
+List name= new List<int>(2);
+name[1] = 'Nicola';
+name[2] = 'Tesla';
+print(name);
+
+//map
+Map dct = new Map();
+dct['dart'] = 'data types';
+dct['flutter'] = 'DVM';
+print(dct)
